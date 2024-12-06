18bb1a0ab6fe82eb3d220ed53214cc286f911abb
Sadfahlsdj <124646333+Sadfahlsdj@users.noreply.github.com>
2024-12-06 13:10:38 -0500

    tried to use threading to speed up d6p2
    did not work

--- Day6/Day6_p2_threading.py
+++ Day6/Day6_p2_threading.py
@@ -1,3 +1,7 @@
+""" attempt to speed up d6p2 with multithreading"""
+import threading
+import concurrent.futures
+import copy
 import time
 
 file_to_use = 'input.txt'
@@ -26,10 +30,19 @@ for i in range(len(lines)):
 DIRS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
 
-
-def cycle(guard_pos, grid):
-    visited_directed = set()  # list of tuples from source -> target positions
+guard_pos_og = guard_pos
+def cycle(args):
+    visited_directed = set()  # set of tuples from source -> target positions
     dir_index = 0
+
+    guard_pos = guard_pos_og
+    grid_to_use = copy.deepcopy(grid_chars)
+
+    x, y = args[0], args[1]
+    if grid_to_use[x][y] == '.':
+        grid_to_use[x][y] = '#'
+    else:
+        return False
 
     while True:
         visited_key = tuple([tuple(guard_pos), dir_index])
@@ -41,7 +54,7 @@ def cycle(guard_pos, grid):
         if (temp[0] < 0 or temp[0] > (len(lines) - 1)
             or temp[1] < 0 or temp[1] > (len(lines[0]) - 1)):
             return False
-        if grid[temp[0]][temp[1]] == '#':
+        if grid_to_use[temp[0]][temp[1]] == '#':
             dir_index = (dir_index + 1) % len(DIRS)
         else:
             guard_pos = temp
@@ -49,11 +62,9 @@ def cycle(guard_pos, grid):
 
 looping = 0
 
-guard_pos_og = guard_pos
-iteration = 0
 
-start = time.time()
-for i in range(len(grid)):
+iteration = 0
+"""for i in range(len(grid)):
     for j in range(len(grid[0])):
         if grid_chars[i][j] == '.':
             grid_chars[i][j] = '#'
@@ -61,7 +72,20 @@ for i in range(len(grid)):
             grid_chars[i][j] = '.'
 
         iteration += 1
-        print(f'iteration: {iteration} looping: {looping}')
+        print(f'iteration: {iteration} looping: {looping}')"""
+
+start = time.time()
+args = []
+for i in range(len(grid)):
+    for j in range(len(grid[0])):
+        args.append(tuple([i, j]))
+
+iter = 0
+with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+    for _ in executor.map(cycle, args):
+        iter += 1
+        looping += _
+        print(f'iter: {iter} loop: {looping}')
 
 print(looping)
 print(f'time: {time.time() - start}')
