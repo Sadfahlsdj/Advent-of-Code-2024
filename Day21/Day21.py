f92dace3f80d1c583c5cdd13364a8750cb808a9a
Sadfahlsdj <124646333+Sadfahlsdj@users.noreply.github.com>
2024-12-21 23:21:39 -0800

    tried recursing on d21p1, (shocker) still not working

--- Day21/Day21.py
+++ Day21/Day21.py
@@ -1,18 +1,11 @@
 import copy
 import itertools
+import networkx as nx
 
 with open('example.txt') as f:
     lines = [l.strip() for l in f.readlines()]
 
-def find_char(c, l):
-    for i, sublist in enumerate(l):
-        for j, ch in enumerate(sublist):
-            if ch == c:
-                return (i, j)
-
-    return 'not found'
-
-def find_all_paths(src, targ):
+def find_all_paths(src, targ, grid):
     """
 
     :param src: source coords as tuple(x, y)
@@ -35,70 +28,57 @@ def find_all_paths(src, targ):
 
     return [list(l) for l in list(set(itertools.permutations(moves)))]
 
-def generate_path(target_grid, input_list, curr, dtype, memo):
+def generate_path(target_map, target_grid, input_list, curr, possible):
     """
 
-    :param target_grid: grid to search for chars on
+    :param target_map: map listing which chars are in which position in a grid
     :param l: list of possible input sequences to search on
     :param curr: starting position
-    :param dtype: int or str to decide whether to cast or not
+    :param possible: possible outcomes at this stage
+    :param iter: crap for diagnostics
     :return: all possible output strings given all possible input strings
     """
-    seq = []
-    for ind, l in enumerate(input_list):
-        if tuple(l) in memo:
-            seq += memo[l]
-        else:
-            temp = [[]]
-            for i, char in enumerate(l):
-                temp2 = []
-                if dtype == 'int':
-                    target_pos = find_char(int(char), target_grid)
-                else:
-                    target_pos = find_char(char, target_grid)
+    if len(input_list) == 0:
+        return possible
 
-                if curr == target_pos:
-                    possible = [['A']]
-                else:
-                    possible = find_all_paths(curr, target_pos)
-                    possible = [p + ['A'] for p in possible]
+    temp, temp2 = [], []
+    target_pos = target_map[input_list[0]]
+    if curr == target_pos:
+        temp = [['A']]
+    else:
+        po = find_all_paths(curr, target_pos, target_grid)
+        temp = [p + ['A'] for p in po]
 
-                for t in temp:
-                    for p in possible:
-                        temp2.append(t + p)
+    for t in temp:
+        for p in possible:
+            temp2.append(p + t)
 
+    return generate_path(target_map, target_grid, input_list[1:], target_pos, temp2)
 
-                temp = temp2
-                curr = target_pos
-
-            memo[tuple(l)] = temp
-            seq += temp
-        """min_len = min([len(x) for x in seq])
-        seq = [s for s in seq if len(s) == min_len]"""
-        print(f'iteration {ind} of {len(input_list)}, seq has len {len(seq)}')
-
-    min_len = min([len(x) for x in seq])
-    seq = [s for s in seq if len(s) == min_len]
-    return seq
 
 def main():
+    n_map = {'7': (0, 0), '8': (0, 1), '9': (0, 2), '4': (1, 0), '5': (1, 1), '6': (1, 2),
+             '1': (2, 0), '2': (2, 1), '3': (2, 2), '0': (3, 1), 'A': (3, 2)}
+    d_map = {'^': (0, 1), 'A': (0, 2), '<': (1, 0), 'v': (1, 1), '>': (1, 2)}
     g_numbers = [[7, 8, 9], [4, 5, 6], [1, 2, 3], [-1, 0, 1000]]
     g_dir = [['-', '^', 'A'], ['<', 'v', '>']]
+
+    # NETWORKX GRAPH CREATION IGNORE IGNORE IGNORE
     memo = {}
 
     total = 0
     for i, l in enumerate(lines):
         initial_n, initial_g = (3, 2), (0, 2)
         seq_n = list(l)
-        seq_n[seq_n.index('A')] = 1000
         seq_n = [seq_n]
         curr_n, curr_g = initial_n, initial_g
 
-        seq_g1 = generate_path(g_numbers, seq_n, curr_n, 'int', memo)
-        print('g1 done')
-        seq_g2 = generate_path(g_dir, seq_g1, copy.deepcopy(curr_g), 'str', memo)
-        print('g2 done')
-        seq_g3 = generate_path(g_dir, seq_g2, copy.deepcopy(curr_g), 'str', memo)
+        # returns a list nested 3 times instead of 2 times, so take 0th ind in each step
+        seq_g1 = [generate_path(n_map, g_numbers, s, curr_n, [[]]) for s in seq_n][0]
+        seq_g2 = [generate_path(d_map, g_dir, s, copy.deepcopy(curr_g), [[]])
+                  for s in seq_g1][0]
+        seq_g3 = [generate_path(d_map, g_dir, s, copy.deepcopy(curr_g), [[]],
+                    ) for s in seq_g2][0]
 
         shortest_sublist = min(seq_g3, key=len)
         # print((''.join([c for c in longest_sublist])))
